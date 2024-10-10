41cba7b0fb474d24a0787f88417b60327cc1dfb1
rinmarys <rinmarysmonagas@gmail.com>
2024-10-09 23:08:46 -0300

    Front-end do blog terminado

---
+++ OncoMed-Blog/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div>
+      <div className='footer-container'>
+        <h1>oi</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
