29845a9fc5d4e3722b608dee8d51d5a459a4ddd1
rinmarys <rinmarysmonagas@gmail.com>
2024-10-24 17:28:00 -0300

    Atualizações na tela de criar postagem

--- OncoMed-Blog/src/routes/Router.jsx
+++ OncoMed-Blog/src/routes/Router.jsx
@@ -3,12 +3,14 @@ import BlogInicio_Admin from "../pages/BlogInicio_Admin";
 import BlogInicio from "../pages/BlogInicio_Paciente";
 
 import ConteudoBlog from "../pages/ConteudoBlog";
+import CriarPostagem from "../pages/CriarPostagem";
 
 const router = createBrowserRouter(
 [
     {path: "/", element: <BlogInicio />},
     {path: "/conteudoBlog", element: <ConteudoBlog />},
     {path: "/blogInicioAdmin", element: <BlogInicio_Admin />},
+    {path: "/criarPostagem", element: <CriarPostagem />},
 ]
 )
 
