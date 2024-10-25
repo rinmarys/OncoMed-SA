b05dd2b7eb2840e547f94c1546319073867c6d6e
artissanvic <maria_vc_ferreira@estudante.sesisenai.org.br>
2024-10-25 13:57:27 -0300

    fazewndo telas de solicitacao de consiulta

--- OncoMed-mavi/src/pages/SolicitacaoConsulta.jsx
+++ OncoMed-mavi/src/pages/SolicitacaoConsulta.jsx
@@ -1,33 +1,3 @@
-// import React from 'react'
-// import './SolicitacaoConsulta.css'
-
-// function SolicitacaoConsulta() {
-//     return (
-//         <div>
-
-//             <div className='tudo-sol'>
-//                 <img className='logo-sol' src="Logo SA.svg" alt="" />
-//                 <div className="alinhamento-sol">
-//                     <div className="titulo-sol">
-//                         <h1>SOLICITAÇÕES DE CONSULTA</h1>
-//                         <div className="linha-sol"></div> 
-//                     </div>
-
-//                 </div>
-
-//             </div>
-//             <div className="subtitulo-sol">
-//                         <h2>SOLICITAÇÕES NÃO LIDAS</h2>
-//                     </div>
-
-//         </div>
-
-//   )
-// }
-
-// export default SolicitacaoConsulta
-
-
 import React from 'react'
 import './SolicitacaoConsulta.css'
 
@@ -44,29 +14,28 @@ function SolicitacaoConsulta() {
                 <p>SOLICITAÇÕES NÃO LIDAS</p>
             </div>
 
-
+<div className="consultas-solicitacao">
             <div className="consulta-solicitada">
 
-                <div className="nome-data-solicitacao">
-                    <h1 className='nome-pessoa-solicitacao' >MARILENE JUSSARA PESSOA</h1>
-                    <h2 className='data-solicitacao' >09/12/2024</h2>
-                </div>
-                <div className="tipo-horario-solicitacao">
-                    <h2 className='tipo-consulta-solicitacao' >MAMOGRAFIA</h2>
-                    <h2 className='horario-consulta-solicitacao' >12:30</h2>
+                <div className="alinhamento-solicitacao-nome-horario-tipo-data">
+                    <div className="nome-tipo-solicitacao">
+                        <h1 className='nome-pessoa-solicitacao' >MARILENE JUSSARA PESSOA</h1>
+                        <h2 className='tipo-consulta-solicitacao' >MAMOGRAFIA</h2>
+                    </div>
+                    <div className="data-horario-solicitacao">
+                        <h2 className='data-solicitacao' >09/12/2024</h2>
+                        <h2 className='horario-consulta-solicitacao' >12:30</h2>
+                    </div>
                 </div>
                 <h2 className='observacao-solicitacao-titulo' >OBSERVAÇÃO</h2>
-                <h3 className='observacao-solicitacao-cliente' >tenho medo mto medo</h3>
+                <h3 className='observacao-solicitacao-cliente' >tenho medo mto medo, meu bcacjkjhdsakh socosadjaukjsdhkj a a a  ja aiii</h3>
                 <h3 className='designar-profissional-titulo' >DESIGNAR PROFISSIONAL</h3>
-
                 <div className="designar-botoes-solicitacao">
                     <select id="escolha-medico" className='designar-profissional-input'>
-                        
-                            <option value="" disabled selected></option>
-                            <option value="medico1">WALTER CAMARGO</option>
-                            <option value="medico2">ANGELICA FREITAS</option>
-                            <option value="medico3">GUSTAVO SANTOS</option>
-                     
+                        <option value="" disabled selected></option>
+                        <option value="medico1">WALTER CAMARGO</option>
+                        <option value="medico2">ANGELICA FREITAS</option>
+                        <option value="medico3">GUSTAVO SANTOS</option>
                     </select>
                     <div className="botoes-solicitacao">
                         <button className='botao-confirma-solicitacao'>CONFIRMAR</button>
@@ -76,7 +45,8 @@ function SolicitacaoConsulta() {
 
             </div>
 
-            <img src="haker.webp" alt="" />
+       
+            </div>
 
         </div>
     )
