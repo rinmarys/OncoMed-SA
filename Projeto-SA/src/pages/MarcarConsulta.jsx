87939f209f3463ed98b17a3794ed25c09dbd68ad
rinmarys <rinmarysmonagas@gmail.com>
2024-11-04 15:29:18 -0300

    AHHHHHHH FUNCIONAAAA

--- Projeto-SA/src/pages/MarcarConsulta.jsx
+++ Projeto-SA/src/pages/MarcarConsulta.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './MarcarConsulta.css'
 import HamburguerMenu from '../components/HamburgerMenu.jsx'
 import Scheduler from '../components/Scheduler.jsx'
+import { Link } from 'react-router-dom'
 
 
 function MarcarConsulta() {
