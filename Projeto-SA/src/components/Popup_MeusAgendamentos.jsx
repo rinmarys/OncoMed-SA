dd14640e164c14c150861efc208dbbc4392d6ad3
MARIA VICTÓRIA DA CRUZ FERREIRA <maria_vc_ferreira@estudante.sesisenai.org.br>
2024-11-06 17:23:13 -0300

    fazendo tela de meus agendamentos

--- Projeto-SA/src/components/Popup_MeusAgendamentos.jsx
+++ Projeto-SA/src/components/Popup_MeusAgendamentos.jsx
@@ -9,11 +9,16 @@ function Popup_MeusAgendamentos() {
       <div className="cada-agendamento-meusagendamentos">
         <div className="todas-infos-meusagendamentos">
           <h1 className='medico-meusagendamentos' >Dr. Jossian Castanho</h1>
-          <h2 className='tipo-consulta-meusagendamentos'>MAMOGRAFIA</h2>
-          <h2 className='horario-meusagendamentos'>12:45</h2>
+
+          <div className="tipo-horario-meusagendamentos">
+            <h2 className='tipo-consulta-meusagendamentos'>MAMOGRAFIA</h2>
+            <h2 className='horario-meusagendamentos'>12:45</h2>
+          </div>
+
           <h3 className='titulo-observacao-meusagendamentos'>SUA OBSERVAÇÃO</h3>
           <h3 className='observacao-meusagendamentos'>isso aquilo aqule outro, paaa amo nuggets</h3>
 
+          <button className='cancelar-meusagendamentos-popup'>CANCELAR</button>
         </div>
       </div>
 
--- Projeto-SA/src/pages/MeusAgendamentos.jsx
+++ Projeto-SA/src/pages/MeusAgendamentos.jsx
@@ -3,17 +3,21 @@ import Scheduler from '../components/Scheduler'
 import HamburguerMenu from '../components/HamburgerMenu'
 import Popup_MeusAgendamentos from '../components/Popup_MeusAgendamentos'
 import CalendarioMeusAgendamentos from '../components/CalendarioMeusAgendamentos'
+import './MeusAgendamentos.css'
 function MeusAgendamentos() {
   return (
     <div className='tudo-MeusAgendamentos'>
       
       <HamburguerMenu />
 
+<div className="alinhar-meusagendamentos">
+      <img className='imagem-meusagendamentos' src="imagem_meusagendamentos.svg" alt="" />
       <div className="calendario">
        <CalendarioMeusAgendamentos/>
       </div>
-
+      </div>
     <Popup_MeusAgendamentos/>
+
 
 
 
