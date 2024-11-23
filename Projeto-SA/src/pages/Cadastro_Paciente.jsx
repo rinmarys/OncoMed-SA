5552daca759801a91ce4d5e3bd0c18fb4d01611c
nicholas-sc-08 <nicholassc.2008@gmail.com>
2024-11-23 17:26:20 -0300

    Informações de marcar a consulta já estão indo pro banco

--- Projeto-SA/src/pages/Cadastro_Paciente.jsx
+++ Projeto-SA/src/pages/Cadastro_Paciente.jsx
@@ -147,7 +147,7 @@ function Cadastro_Paciente() {
             fetch_pacientes(); // Atualiza a lista de pacientes após a edição
             setForm({ nome: '', cpf: '', cep: '', email: '', genero: '', data_de_nascimento: '', senha: '' }); // Limpa o formulário
             setSelectedCliente(null); // Reseta o paciente selecionado
-          }
+          };
         } else {
           // Adicionar novo cliente (POST)
           const response = await axios.post('http://localhost:3000/pacientes', form);
