42c0e97690bef00727ccc46d51ac36632e25f4f8
rinmarys <rinmarysmonagas@gmail.com>
2024-11-23 23:05:21 -0300

    Atualizações nas telas de perfis

--- Projeto-SA/src/pages/MarcarConsulta.jsx
+++ Projeto-SA/src/pages/MarcarConsulta.jsx
@@ -139,165 +139,162 @@ function MarcarConsulta() {
   };
 
 
-}
 
-return (
 
-  <div className='Container-marcarConsulta-alinhamento'>
-    {/* <form onSubmit={handleSubmit}> */}
+  return (
 
-    <div className="alinhamento-hamburger">
-      <HamburguerMenu />
-    </div>
+    <div className='Container-marcarConsulta-alinhamento'>
 
-    <div className="alinhamento-displayFlex">
-
-      <Scheduler />
-
-      <div className="alinhamento-inputs">
-        <div className='Tres-partes'>
-          <div className='Container-Consulta-Horario'>
-            <div className='Selecionar-Consulta'>
-              {/* CONSULTAS E EXAMES À REALIZAR */}
-              <h2>CONSULTAS E EXAMES À REALIZAR</h2>
-              <select className='Select-Consulta'
-                value={consultaSelecionada}
-                onChange={(event) => { setConsultaSelecionada(event.target.value) }}>
-                <option value="" disabled>Selecione um tipo de consulta</option>
-                <option value="consulta1">Mamografia</option>
-                <option value="consulta2">Exame de sangue</option>
-                <option value="consulta3">Consultas bakanas</option>
-              </select>
-            </div>
-            {/* CONSULTAS E EXAMES À REALIZAR */}
-
-            {/* SELECIONAR HORARIO */}
-            <div className='Selecionar-Horario'>
-              <h2>HORÁRIO</h2>
-              <select className="Select-Horario"
-                id="opcao-horario"
-                value={horarioSelecionado}
-                onChange={(event) => { setHorarioSelecionado(event.target.value) }}
-              >
-                <option value="" disabled>Selecione um horario</option>
-                <option value="horario1">12:30</option>
-                <option value="horario2">16:20</option>
-                <option value="horario3">20:10</option>
-              </select>
-            </div>
-            {/* SELECIONAR HORARIO */}
-
-          </div>
-          {/* OBSERVAÇÕES */}
-          <div className='Selecionar-Ob'>
-            <h2>OBSERVAÇÕES</h2>
-            <textarea
-              id='observacao-usuario'
-              className='Input-Ob'
-              type="text"
-
-              value={observacaoEscrita}
-              onChange={(event) => { setObservacaoEscrita(event.target.value) }} />
-          </div>
-          {/* OBSERVAÇÕES */}
-
-          <div className='Botões'>
-            <button className='Button-cancelar' onClick={buttonCancelar} type='button' >CANCELAR</button>
-            <button className='Button-confirmar' onClick={buttonConfirmar} type='submit' >CONFIRMAR</button>
-          </div>
-
-          <div className="mensagem-erro-marcarConsulta">
-            {mensagemErroMarcarConsulta}
-          </div>
-        </div>
+      <div className="alinhamento-hamburger">
+        <HamburguerMenu />
       </div>
 
-      {/* POP UP */}
+      <div className="alinhamento-displayFlex">
 
-      {/* pop up de confirmar */}
-      {mostrarPopUpConfirmar && (
-        <div className="popup-confirmar">
-          <div className="popup-confirmar-conteudo">
-            <h3>Consulta confirmada com sucesso!</h3>
-            <button onClick={() => navigate('/')}>Fechar</button>
+        <Scheduler />
+
+        <div className="alinhamento-inputs">
+          <div className='Tres-partes'>
+            <div className='Container-Consulta-Horario'>
+              <div className='Selecionar-Consulta'>
+                {/* CONSULTAS E EXAMES À REALIZAR */}
+                <h2>CONSULTAS E EXAMES À REALIZAR</h2>
+                <select className='Select-Consulta'
+                  value={consultaSelecionada}
+                  onChange={(event) => { setConsultaSelecionada(event.target.value) }}>
+                  <option value="" disabled>Selecione um tipo de consulta</option>
+                  <option value="consulta1">Mamografia</option>
+                  <option value="consulta2">Exame de sangue</option>
+                  <option value="consulta3">Consultas bakanas</option>
+                </select>
+              </div>
+              {/* CONSULTAS E EXAMES À REALIZAR */}
+
+              {/* SELECIONAR HORARIO */}
+              <div className='Selecionar-Horario'>
+                <h2>HORÁRIO</h2>
+                <select className="Select-Horario"
+                  id="opcao-horario"
+                  value={horarioSelecionado}
+                  onChange={(event) => { setHorarioSelecionado(event.target.value) }}
+                >
+                  <option value="" disabled>Selecione um horario</option>
+                  <option value="horario1">12:30</option>
+                  <option value="horario2">16:20</option>
+                  <option value="horario3">20:10</option>
+                </select>
+              </div>
+              {/* SELECIONAR HORARIO */}
+
+            </div>
+            {/* OBSERVAÇÕES */}
+            <div className='Selecionar-Ob'>
+              <h2>OBSERVAÇÕES</h2>
+              <textarea
+                id='observacao-usuario'
+                className='Input-Ob'
+                type="text"
+
+                value={observacaoEscrita}
+                onChange={(event) => { setObservacaoEscrita(event.target.value) }} />
+            </div>
+            {/* OBSERVAÇÕES */}
+
+            <div className='Botões'>
+              <button className='Button-cancelar' onClick={buttonCancelar} type='button' >CANCELAR</button>
+              <button className='Button-confirmar' onClick={buttonConfirmar} type='submit' >CONFIRMAR</button>
+            </div>
+
+            <div className="mensagem-erro-marcarConsulta">
+              {mensagemErroMarcarConsulta}
+            </div>
           </div>
         </div>
-      )}
-      {/* pop up de confirmar */}
 
-      {mostrarPopUpCancelar && (
-        <div className="popup-cancelar">
-          <div className="popup-cancelar-conteudo">
-            <h3>Você tem certeza que quer cancelar a consulta?</h3>
+        {/* POP UP */}
 
-            <div className="buttons-popupCancelar-alinhamento">
-              <button onClick={() => setMostrarPopUpCancelar(false)} className='popup-cancelar-fechar-button' type='button'>Não quero! Fechar</button>
-              <button onClick={() => navigate('/')} className='popup-cancelar-confirmar-button' type='button'>Confirmar cancelamento</button>
+        {/* pop up de confirmar */}
+        {mostrarPopUpConfirmar && (
+          <div className="popup-confirmar">
+            <div className="popup-confirmar-conteudo">
+              <h3>Consulta confirmada com sucesso!</h3>
+              <button onClick={() => navigate('/')}>Fechar</button>
+            </div>
+          </div>
+        )}
+        {/* pop up de confirmar */}
 
-              <div className="alinhamento-inputs">
-                <div className='Tres-partes'>
-                  <div className='Container-Consulta-Horario'>
-                    <div className='Selecionar-Consulta'>
+        {mostrarPopUpCancelar && (
+          <div className="popup-cancelar">
+            <div className="popup-cancelar-conteudo">
+              <h3>Você tem certeza que quer cancelar a consulta?</h3>
+
+              <div className="buttons-popupCancelar-alinhamento">
+                <button onClick={() => setMostrarPopUpCancelar(false)} className='popup-cancelar-fechar-button' type='button'>Não quero! Fechar</button>
+                <button onClick={() => navigate('/')} className='popup-cancelar-confirmar-button' type='button'>Confirmar cancelamento</button>
+
+                <div className="alinhamento-inputs">
+                  <div className='Tres-partes'>
+                    <div className='Container-Consulta-Horario'>
+                      <div className='Selecionar-Consulta'>
+                        {/* CONSULTAS E EXAMES À REALIZAR */}
+                        <h2>CONSULTAS E EXAMES À REALIZAR</h2>
+                        <select className='Select-Consulta'
+                          value={consultaSelecionada}
+                          onChange={(event) => { setConsultaSelecionada(event.target.value) }}>
+                          <option value="" disabled>Selecione um tipo de consulta</option>
+                          <option value="consulta1">Mamografia</option>
+                          <option value="consulta2">Exame de sangue</option>
+                          <option value="consulta3">Consultas bakanas</option>
+                        </select>
+
+                      </div>
                       {/* CONSULTAS E EXAMES À REALIZAR */}
-                      <h2>CONSULTAS E EXAMES À REALIZAR</h2>
-                      <select className='Select-Consulta'
-                        value={consultaSelecionada}
-                        onChange={(event) => { setConsultaSelecionada(event.target.value) }}>
-                        <option value="" disabled>Selecione um tipo de consulta</option>
-                        <option value="consulta1">Mamografia</option>
-                        <option value="consulta2">Exame de sangue</option>
-                        <option value="consulta3">Consultas bakanas</option>
-                      </select>
+
+                      {/* SELECIONAR HORARIO */}
+                      <div className='Selecionar-Horario'>
+                        <h2>HORÁRIO</h2>
+                        <input type='time' className="Select-Horario"
+                          id="opcao-horario"
+                          value={horarioSelecionado}
+                          onChange={(event) => { setHorarioSelecionado(event.target.value) }}
+                        >
+                        </input>
+                      </div>
+                      {/* SELECIONAR HORARIO */}
 
                     </div>
-                    {/* CONSULTAS E EXAMES À REALIZAR */}
+                    {/* OBSERVAÇÕES */}
+                    <div className='Selecionar-Ob'>
+                      <h2>OBSERVAÇÕES</h2>
+                      <textarea
+                        id='observacao-usuario'
+                        className='Input-Ob'
+                        type="text"
 
-                    {/* SELECIONAR HORARIO */}
-                    <div className='Selecionar-Horario'>
-                      <h2>HORÁRIO</h2>
-                      <input type='time' className="Select-Horario"
-                        id="opcao-horario"
-                        value={horarioSelecionado}
-                        onChange={(event) => { setHorarioSelecionado(event.target.value) }}
-                      >
-                      </input>
+                        value={observacaoEscrita}
+                        onChange={(event) => { setObservacaoEscrita(event.target.value) }} />
                     </div>
-                    {/* SELECIONAR HORARIO */}
+                    {/* OBSERVAÇÕES */}
 
-                  </div>
-                  {/* OBSERVAÇÕES */}
-                  <div className='Selecionar-Ob'>
-                    <h2>OBSERVAÇÕES</h2>
-                    <textarea
-                      id='observacao-usuario'
-                      className='Input-Ob'
-                      type="text"
+                    <div className='Botões'>
+                      <button className='Button-cancelar' onClick={buttonCancelar} type='button' >CANCELAR</button>
+                      <button className='Button-confirmar' onClick={buttonConfirmar} type='submit' >CONFIRMAR</button>
+                    </div>
 
-                      value={observacaoEscrita}
-                      onChange={(event) => { setObservacaoEscrita(event.target.value) }} />
-                  </div>
-                  {/* OBSERVAÇÕES */}
+                    <div className="mensagem-erro-marcarConsulta">
+                      {mensagemErroMarcarConsulta}
 
-                  <div className='Botões'>
-                    <button className='Button-cancelar' onClick={buttonCancelar} type='button' >CANCELAR</button>
-                    <button className='Button-confirmar' onClick={buttonConfirmar} type='submit' >CONFIRMAR</button>
-                  </div>
-
-                  <div className="mensagem-erro-marcarConsulta">
-                    {mensagemErroMarcarConsulta}
-
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-
-
           </div>
-          {/* </form> */}
-        </div>
-      )
-      }
+        )
+        }
+      </div>
     </div>
-  </div>
-)
+  )
+}
 export default MarcarConsulta
