110f06f1c3852561ad5fdc924e9a37546d2a7501
MARIA EDUARDA KLIN DE CARVALHO <maria_ek_carvalho@estudante.sesisenai.org.br>
2024-12-03 14:48:41 -0300

    atualizado telas de Politica/Termos

--- Projeto-SA/src/pages/TermosDeUso.jsx
+++ Projeto-SA/src/pages/TermosDeUso.jsx
@@ -6,44 +6,65 @@ import { Link } from "react-router-dom"
 
 function TermosUso() {
    return (
-      <div> <Header />
-         <div className='Termos-Uso'>
-            <div className="titulo-termosUso">
-               <h1>TERMOS DE USO</h1>
-               <div className='Linha-Dois'></div>
-               <Link className='link-OutraPag' to={'/politicaPrivacidade'}>POLÍTICA DE PRIVACIDADE</Link>
-            </div>
+      <div className='Nav-Bar'>
+      <Header />
 
-            <div className='Texto-Um'>
-               <h2>ABCDEFGH</h2>
+      <div className='Politica-Privacidade'>
+        <h1>TERMOS DE USO</h1>
+        <div className='Linha-Dois'></div>
+        <Link className='link-termosUso' to='/politicaPrivacidade'>POLÍTICA E PRIVACIDADE</Link>
 
-               <p>Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown printer
-                  took a galley of type and scrambled it to make a type specimen book.
-                  It has survived not only five centuries, but also the leap into electronic
-                  typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                  with the release of Letraset sheets containing Lorem Ipsum passages,
-                  and more recently with desktop publishing software like Aldus PageMaker
-                  including versions of Lorem Ipsum.</p>
-            </div>
+        <div className='Texto-Um'>
+          <p>Bem-vindo ao site da Clínica Médica OncoMed. Ao acessar e utilizar nosso site,
+            você concorda com os seguintes termos e condições. Se não concordar com estes termos,
+            por favor, não utilize nosso site.</p>
+        </div>
 
-            <div className='Texto-Dois'>
-               <h2>ABCDEFGH</h2>
+        <div className='Texto-Dois'>
+          <h2 className="Title-ColetaInfo">1. Aceitação dos Termos</h2>
+          <p>Ao acessar nosso site, você declara que tem pelo menos 18 anos de idade
+             ou que está utilizando o site sob a supervisão de um responsável legal.
+             Você concorda em cumprir todos os termos e condições aqui descritos.</p>
+        </div>
 
-               <p>Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown printer
-                  took a galley of type and scrambled it to make a type specimen book.
-                  It has survived not only five centuries, but also the leap into electronic
-                  typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                  with the release of Letraset sheets containing Lorem Ipsum passages,
-                  and more recently with desktop publishing software like Aldus PageMaker
-                  including versions of Lorem Ipsum.</p>
-            </div>
-         </div>
-         {<Footer />}
+        <div className="Texto-Tres">
+          <h2 className="Title-Info">2. Uso do Site</h2>
+          <p className="Text-Info">O conteúdo deste site é destinado a informações gerais
+           sobre os serviços oferecidos pela Clínica Médica OncoMed. Você se compromete 
+           a utilizar o site apenas para fins legais e de acordo com todas as leis aplicáveis.</p>
+        </div>
+
+        <div className="Texto-Quatro">
+          <h2 className="Title-UsoInfo">3. Propriedade Intelectual</h2>
+          <p className="Text-UsoInfo">Todo o conteúdo deste site, incluindo textos, gráficos, 
+          logotipos, ícones e imagens, é de propriedade da Clínica Médica OncoMed ou de seus 
+          licenciadores e está protegido por leis de direitos autorais e propriedade intelectual. 
+          É proibida a reprodução, distribuição ou modificação do conteúdo sem autorização prévia por escrito.</p>
+        </div>
+
+        <div className="Texto-Cinco">
+          <h2 className="Title-UsoInfo">4. Limitação de Responsabilidade</h2>
+          <p className="Text-UsoInfo">A Clínica Médica OncoMed não se responsabiliza por qualquer dano direto ou 
+          indireto resultante do uso do site, incluindo, mas não se limitando a, erros, omissões ou interrupções nos serviços.</p>
+        </div>
+
+        <div className="Texto-Seis">
+          <h2 className="Title-UsoInfo">5. Links Externos</h2>
+          <p className="Text-UsoInfo">Este site pode conter links para sites de terceiros.
+           Esses links são fornecidos apenas para sua conveniência. A Clínica Médica OncoMed 
+           não se responsabiliza pelo conteúdo ou pela precisão de sites de terceiros.</p>
+        </div>
+
+        <div className="Texto-Sete">
+          <h2 className="Title-UsoInfo">6. Agendamento de Consultas</h2>
+          <p className="Text-UsoInfo">Os agendamentos de consultas feitos através do site estão
+           sujeitos à disponibilidade. A Clínica Médica OncoMed reserva-se o direito de alterar 
+           ou cancelar consultas, comunicando o usuário previamente.</p>
+        </div>
       </div>
+
+      {<Footer />}
+    </div>
    )
 }
 
