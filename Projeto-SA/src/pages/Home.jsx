dcccc9f6dd7a56ad859a0b00c771c671f6b88951
NICHOLAS SERENCOVICH CARVALHO <nicholas_carvalho@estudante.sesisenai.org.br>
2024-12-04 16:24:19 -0300

    Conserto da validação dos cadastros

--- Projeto-SA/src/pages/Home.jsx
+++ Projeto-SA/src/pages/Home.jsx
@@ -11,11 +11,7 @@ import Aritgo_Um_Home from '../components/Aritgo_Um_Home.jsx';
 import Artigo_Dois_Home from '../components/Artigo_Dois_Home.jsx';
 import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../components/Footer.jsx';
-<<<<<<< HEAD
 import axios from 'axios';
-=======
-import axios from "axios";
->>>>>>> 11ecf992579b9f346faa3d2aab44dc8bfb915f88
 
 import Pop_up_de_boas_vindas from '../components/Pop_up_de_boas_vindas.jsx';
 
@@ -283,7 +279,7 @@ function Home() {
 
             </ul>
 
-            <p>Atendimento disponível de segunda a sexta, das 8h às 18h</p>
+            <p>Atendimento disponível de segunda a sexta, 24Hrs!</p>
 
           </div>
 
